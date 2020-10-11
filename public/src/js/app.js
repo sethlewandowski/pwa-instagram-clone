1feec8bf7ddf7e417f26019d023cab70b9e04d7a
sethl <lewandowski.seth@gmail.com>
2020-10-10 17:39:36 -0700

    saving work again

--- public/src/js/app.js
+++ public/src/js/app.js
@@ -1,6 +1,15 @@
+let deferredPrompt;
+
 // check if sw is available in user browser
 if ('serviceWorker' in navigator) {
   navigator.serviceWorker
     .register('/sw.js')
     .then(() => console.log('service worker registered.'));
 }
+
+window.addEventListener('beforeinstallprompt', (event) => {
+  console.log('beforeinstallprompt fired');
+  event.preventDefault;
+  deferredPrompt = event;
+  return false;
+});

89cd4f918b0b674311402eb04d055bd359f8dd24
sethl <lewandowski.seth@gmail.com>
2020-10-10 17:05:05 -0700

    saving work

--- public/sw.js
+++ public/sw.js
@@ -6,3 +6,8 @@ self.addEventListener('activate', (e) => {
   console.log('[Service Worker] Activating Service Worker ... ', e);
   return self.clients.claim();
 });
+
+self.addEventListener('fetch', (e) => {
+  console.log('[Service Worker] Fetching something...', e);
+  e.respondWith(fetch(e.request));
+});
